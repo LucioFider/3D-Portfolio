7f9b2f118abd0a5865e63c98b96da2332cbd4d26
LucioFider <fidelislucio404@gmail.com>
2023-03-01 08:45:20 +1000

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+
+function App() {
+
+  return (
+    <div >
+    React 3D Portfolio
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/index.js
@@ -0,0 +1,6 @@
+import Hero from "./Hero";
+import Contact from "./Contact";
+import Who from "./Who";
+import Works from "./Works";
+
+export { Hero, Contact, Who, Works };
